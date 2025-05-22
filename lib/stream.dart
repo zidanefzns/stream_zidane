33fd963d5a50e6da18efef3fae9e37f2c7c8e61f
Zidane Fauzan <zidanefzn21@gmail.com>
2025-05-22 12:03:45 +0700

    P1: Jawaban Soal 2

---
+++ lib/stream.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> colors = [
+    Colors.lightBlue,
+    Colors.lightGreen,
+    Colors.deepPurpleAccent,
+    Colors.orangeAccent,
+    Colors.pinkAccent,
+  ];
+}
