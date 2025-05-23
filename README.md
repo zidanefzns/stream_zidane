2df48541f63a1427260701905126a8677a8d6fff
Zidane Fauzan <zidanefzn21@gmail.com>
2025-05-23 17:59:20 +0700

    P3: Jawaban Soal 8

--- README.md
+++ README.md
@@ -42,3 +42,13 @@ onError(...) adalah callback untuk menangani error saat mendengarkan stream. Ini
 
 3. Langkah 15
 Method memanggil addError() dari numberStream, yang berarti mengirim error ke dalam stream.
+
+## Praktikum 3
+### Soal 8
+![Capture soal 8](assets/stream3.gif)
+
+1. Langkah 1
+late artinya variabel transformer akan diinisialisasi nanti sebelum digunakan. StreamTransformer adalah objek yang bisa mengubah isi stream sebelum dikonsumsi oleh listen.
+
+2. Langkah 3
+Menerapkan transformasi ke stream. Output dari stream yang ditransformasi akan berbeda sesuai logika transformer tersebut.
--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class StreamHomePage extends StatefulWidget {
 }
 
 class _StreamHomePageState extends State<StreamHomePage> {
+  late StreamTransformer transformer;
   int lastNumber = 0;
   late StreamController numberStreamController;
   late NumberStream numberStream;
@@ -49,18 +50,39 @@ void addRandomNumber() {
 
   @override
   void initState() {
+    transformer = StreamTransformer<int, int>.fromHandlers(
+      handleData: (value, sink) {
+        sink.add(value * 10);
+      },
+      handleError: (error, trace, sink) {
+        sink.add(-1);
+      },
+      handleDone: (sink) => sink.close(),
+    );
     numberStream = NumberStream();
     numberStreamController = numberStream.controller;
     Stream stream = numberStreamController.stream;
-    stream.listen((event) {
+    stream
+        .transform(transformer)
+        .listen((event) {
           setState(() {
             lastNumber = event;
           });
-        }).onError((error) {
+        })
+        .onError((error) {
           setState(() {
             lastNumber = -1;
           });
         });
+    // stream.listen((event) {
+    //       setState(() {
+    //         lastNumber = event;
+    //       });
+    //     }).onError((error) {
+    //       setState(() {
+    //         lastNumber = -1;
+    //       });
+    //     });
     super.initState();
     // colorStream = ColorStream();
     // changeColor();
